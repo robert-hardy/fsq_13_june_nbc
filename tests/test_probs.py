f29c53d5d1697c4d487cf2a5a6dbeab78db37b0c
Robert Hardy <robbert@gmail.com>
2017-06-13 18:24:21 +0000

    Refactor tests to use a fixture.
    All green.

--- tests/test_probs.py
+++ tests/test_probs.py
@@ -1,16 +1,21 @@
+import pytest
+
 from nbc import (
     trained,
     prob
 )
 
 
-def test_good_is_nice():
-    df = trained()
+@pytest.fixture()
+def df():
+    return trained()
+
+
+def test_good_is_nice(df):
     result = prob('good', 'nice', df)
     assert result == 1.0
 
 
-def test_john_is_fifty_fifty():
-    df = trained()
+def test_john_is_fifty_fifty(df):
     result = prob('John', 'nice', df)
     assert result == 0.5
