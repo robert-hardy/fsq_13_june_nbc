1e3cc1d163d27e2479577da9bdc6ec2792823c51
Robert Hardy <robbert@gmail.com>
2017-06-13 18:19:26 +0000

    Add green test.
    All green.

--- tests/test_probs.py
+++ tests/test_probs.py
@@ -8,3 +8,9 @@ def test_good_is_nice():
     df = trained()
     result = prob('good', 'nice', df)
     assert result == 1.0
+
+
+def test_john_is_fifty_fifty():
+    df = trained()
+    result = prob('John', 'nice', df)
+    assert result == 0.5
