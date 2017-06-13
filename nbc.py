3043d94e5a31c84919a3fccc1aa4b802be7a8bcc
Robert Hardy <robbert@gmail.com>
2017-06-13 18:40:06 +0000

    Modify prob.
    Still red, wip.

--- nbc.py
+++ nbc.py
@@ -31,9 +31,9 @@ def prob(f, c, trained):
     if f not in trained.columns:
         return 0.5
     cat = trained['cat']
-    feature = trained[f]
+    feature = trained['feature']
 
-    prob_f_given_c_num = ((cat == c) & (feature == 1)).sum()
+    prob_f_given_c_num = ((cat == c) & (feature == f)).sum()
     prob_f_given_c_den = (cat == c).sum()
     prob_f_given_c = float(prob_f_given_c_num)/prob_f_given_c_den
 
@@ -41,7 +41,7 @@ def prob(f, c, trained):
     prob_c_den = len(cat)
     prob_c = float(prob_c_num)/prob_c_den
 
-    prob_f_num = (feature == 1).sum()
+    prob_f_num = (feature == f).sum()
     prob_f_den = len(feature)
     prob_f = float(prob_f_num)/prob_f_den
 
