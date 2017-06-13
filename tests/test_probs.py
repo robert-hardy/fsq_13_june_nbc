a0b5e34d0092a26bdf86d378de24d37342d6e07a
Robert Hardy <robbert@gmail.com>
2017-06-13 18:25:44 +0000

    Test on an unseen work.
    Fails, wip.

--- tests/test_probs.py
+++ tests/test_probs.py
@@ -19,3 +19,8 @@ def test_good_is_nice(df):
 def test_john_is_fifty_fifty(df):
     result = prob('John', 'nice', df)
     assert result == 0.5
+
+
+def test_foo_is_fifty_fifty(df):
+    result = prob('foo', 'nice', df)
+    assert result == 0.5
