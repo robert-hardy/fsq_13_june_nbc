844866574004de3760e3d67d2508bf95a055dbc3
Robert Hardy <robbert@gmail.com>
2017-06-13 18:31:51 +0000

    Test on word 'cat'.
    Fails, needs a refactor.

--- tests/test_probs.py
+++ tests/test_probs.py
@@ -24,3 +24,7 @@ def test_john_is_fifty_fifty(df):
 def test_foo_is_fifty_fifty(df):
     result = prob('foo', 'nice', df)
     assert result == 0.5
+
+def test_cat_is_fifty_fifty(df):
+    result = prob('cat', 'nice', df)
+    assert result == 0.5
